3debda3a920e94ce456f6bddb087132ef2615ef6
qcjoker3 <119907570+qcjoker3@users.noreply.github.com>
2026-05-07 12:16:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -56,26 +56,4 @@ function answerDiag(questionId, answer) {
 
     feedbackDiv.innerHTML = feedbackHTML;
     feedbackDiv.classList.add('show');
-}            
-        case 'diag-credit':
-            feedbackHTML = answer === 'non' 
-                ? 'Bravo! Vous planifiez très bien vos besoins futurs en liquidités. <a href="credit.html" class="diag-link">En savoir plus →</a>' 
-                : 'Les frais d\'intérêts sont vicieux et détruisent votre patrimoine. <a href="credit.html" class="diag-link">En savoir plus →</a>';
-            break;
-
-        case 'diag-placements':
-            feedbackHTML = answer === 'oui' 
-                ? 'Parfait. Vous avez des placements et connaissez le risque adapté à votre situation. <a href="investissements.html" class="diag-link">En savoir plus →</a>' 
-                : 'Ne pas connaître son profil d\'investisseur c\'est comme naviguer en haute mer sans boussole. c\'est faire un cadeau involontaire à l\'impôt. <a href="investissements.html" class="diag-link">En savoir plus →</a>';
-            break;
-
-        case 'diag-retraite':
-            feedbackHTML = answer === 'oui' 
-                ? 'Félicitations. Avoir un plan financier est la première étape de la création d\'un patrimoine.  <a href="retraite.html" class="diag-link">En savoir plus →</a>' 
-                : 'La plupart des gens sous-estiment le capital nécessaire pour maintenir leur niveau de vie. <a href="retraite.html" class="diag-link">En savoir plus →</a>';
-            break;
-    }
-
-    feedbackDiv.innerHTML = feedbackHTML;
-    feedbackDiv.classList.add('show');
 }
